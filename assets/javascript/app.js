499ff5bb46c70a50da9bef6425da8a34addbcc61
Elisabeth Edrington <edringtondc@gmail.com>
2018-11-23 20:49:07 -0700

    main css added, buttons function and gifs render. No local storage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
 
-    var animalArr = ["alligator", "monkey", "spider"];
+    var topicArr = ["Nintendo", "Mario", "Donkey Kong", "Luigi", "Pokemon", "Zelda", "Yoshi", "Super Smash Bros"];
     
-    console.log(animalArr);
+    console.log(topicArr);
 
     function renderButtons() {
         //empty button div
         $("#button-div").empty();
 
-        animalArr.forEach(function (animal) {
+        topicArr.forEach(function (topic) {
             var a = $("<button>");
             // Adds a class of movie to our button
-            a.addClass("animal");
+            a.addClass("topic btn btn-danger mr-1 ml-1");
             // Added a data-attribute
-            a.attr("data-animal", animal);
+            a.attr("data-topic", topic);
             // Provided the initial button text
             // a.attr("data-state", still)
-            a.text(animal);
+            a.text(topic);
             // Added the button to the buttons-view div
             $("#button-div").append(a);
 
@@ -25,19 +25,19 @@ $(document).ready(function () {
     renderButtons();
 
 
-    $("#add-animal").on("click", function (event) {
+    $("#add-topic").on("click", function (event) {
         event.preventDefault();
         
         // This line of code will grab the input from the textbox
-        var newAnimal = $("#animal-input").val().trim();
+        var newTopic = $("#topic-input").val().trim();
 
         // The movie from the textbox is then added to our array
-        animalArr.push(newAnimal);
+        topicArr.push(newTopic);
 
         // Calling renderButtons which handles the processing of our movie array
         renderButtons();
-        console.log(animalArr);
-        console.log(newAnimal);
+        console.log(topicArr);
+        console.log(newTopic);
         
         
 
@@ -50,9 +50,9 @@ $(document).ready(function () {
         event.preventDefault();
 
         console.log("button clicked");
-        var animal = $(this).attr("data-animal");
+        var topic = $(this).attr("data-topic");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+            topic + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         $.ajax({
             url: queryURL,
@@ -68,28 +68,28 @@ $(document).ready(function () {
             for (var i = 0; i < results.length; i++) {
 
                 // Step 3: uncomment the for loop above and the closing curly bracket below.
-                // Make a div with jQuery and store it in a variable named animalDiv.
-                var animalDiv = $("<div>");
+                // Make a div with jQuery and store it in a variable named topicDiv.
+                var topicDiv = $("<div class='m-1'>");
                 // Make a paragraph tag with jQuery and store it in a variable named p.
                 var p = $("<p>");
                 // Set the inner text of the paragraph to the rating of the image in results[i].
                 p.text("rating: " + results[i].rating);
-                // Make an image tag with jQuery and store it in a variable named animalImage.
-                var animalImage = $("<img>");
+                // Make an image tag with jQuery and store it in a variable named topicImage.
+                var topicImage = $("<img>");
                 // Set the image's src to results[i]'s fixed_height.url.
 
 
-                animalImage.addClass("gif");
-                animalImage.attr("src", results[i].images.fixed_height_still.url);
-                animalImage.attr("data-still", results[i].images.fixed_height_still.url);
-                animalImage.attr("data-animate", results[i].images.fixed_height.url);
-                animalImage.attr("data-state", "still");
-                // Append the p variable to the animalDiv variable.
-                animalDiv.append(p);
-                // Append the animalImage variable to the animalDiv variable.
-                animalDiv.append(animalImage)
-                // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
-                $("#gifs-appear-here").prepend(animalDiv);
+                topicImage.addClass("gif");
+                topicImage.attr("src", results[i].images.fixed_height_still.url);
+                topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+                topicImage.attr("data-animate", results[i].images.fixed_height.url);
+                topicImage.attr("data-state", "still");
+                // Append the p variable to the topicDiv variable.
+                topicDiv.append(p);
+                // Append the topicImage variable to the topicDiv variable.
+                topicDiv.append(topicImage)
+                // Prepend the topicDiv variable to the element with an id of gifs-appear-here.
+                $("#gifs-appear-here").prepend(topicDiv);
 
             }
 
